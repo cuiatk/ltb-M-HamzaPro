5edc9788bc2c3a6b0e7e197ae4c60d9772ca9554
Muhammad hamza <m.hamza7459@gmail.com>
2019-06-13 11:09:43 +0500

    TODO 3 completed

--- src/labTerminalB/Customer.java
+++ src/labTerminalB/Customer.java
@@ -31,7 +31,19 @@ public class Customer {
 		 * Provide suitable specification for statement method 
 		 */
 		
-		
+		/**
+		 * this method is constructing a statement which has name of person, movies being rented, and
+		 * rental points of person.
+		 * It is a method to calculate and print a statement of a customer's charges at a video store.
+         * The method is told which movies a customer rented and for how long.
+         * It then calculates the charges, which depend on how long the movie is rented, and
+         * identifies the type movie. There are three kinds of movies: regular, children's, and new releases.
+         * In addition to calculating charges, the statement also computes frequent renter points, which vary
+		 * depending on whether the film is a new release.
+		 * @return a statement which contain the name of person who is renting movie(s)
+		 * and name of all movies which are being rented by that specific person with their
+		 * calculated prices, and rental points of that person.
+		 */
 	
 		public String statement() {
 			double totalAmount = 0;
